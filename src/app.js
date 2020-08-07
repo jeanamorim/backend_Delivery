752820b726fcdb227804f99119e37b3e9a54428f
jean amorim <jeanamorim08@hotmail.com>
2020-08-07 17:20:49 -0300

    alteração do redis

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(helmet());
+    // this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
