99219cb8e111f189eb566dd9ac7a9f12f0cd11cc
jean amorim <jeanamorim08@hotmail.com>
2020-08-23 16:27:48 -0300

    ok

--- src/database/migrations/20191013183137-create-estabelecimento.js
+++ src/database/migrations/20191013183137-create-estabelecimento.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('estabelecimento', {
+    return queryInterface.createTable('estabelecimentos', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -77,6 +77,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('estabelecimento');
+    return queryInterface.dropTable('estabelecimentos');
   },
 };
